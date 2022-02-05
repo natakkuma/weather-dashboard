6e203ba9f872022765f5fb4404fdee5b9bf64c3e
natakkuma <95733427+natakkuma@users.noreply.github.com>
2022-02-04 22:16:17 -0800

    initial html update

---
+++ assets/script.js
@@ -0,0 +1,42 @@
+//VARIABLES
+
+
+
+
+
+//FUNCTIONS
+
+//Search for City 
+
+
+
+//City Searched - Present with Curent Weather Conditions
+
+    //View Current Weather Conditions 
+    //View City Name
+    //View the Date 
+    //View Icon of Weather Condition
+    //View Icon of Temperature
+    //View Icon of Humidity
+    //View Icon of Wind Speed
+    //View UV Index 
+
+// When UV Index is viewed - Shows Color that indicates whether conditions are favorable, moderate, or severe
+
+//City Searched - Present with Future Weather Conditions
+
+    //View 5 day weather forecast that displays:
+        //View City Name
+        //View the Date 
+        //View Icon of Weather Condition
+        //View Icon of Temperature
+        //View Icon of Humidity
+        //View Icon of Wind Speed
+
+
+
+//City Searched - Added to Search History (Local Storage) 
+
+//Show Present and Future Weather conditions when City is Selected from Search History
+
+
